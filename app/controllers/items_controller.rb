a8babf9f1c90d452bb881aa1e68b85da6746d2d1
Nancy Zaky <nancyzki@yahoo.com>
2021-10-06 16:51:31 -0700

    Assignment complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      if user
+      items = user.items
+      else
+        return  render json: {error: "user not found"}, status: :not_found
+      end
+    else
     items = Item.all
-    render json: items, include: :user
   end
+    render json: items, include: :user
 
+end
+def create
+  user = User.find(params[:user_id])
+  item = user.items.create(item_params)
+  render json: item, status: :created
+
+end
+def show
+  item = Item.find(params[:id])
+  render json: item
+end
+
+
+
+private
+def render_not_found
+  render json: {error: "user not found"}, status: :not_found
+end
+def item_params
+  params.permit(:name, :description, :price)
+end
 end
